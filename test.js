c60dcf2291909c5333664ddae9d490ab5ddde863
vitalikod <vitalikod879@gmail.com>
2022-07-29 09:01:43 +0300

    qaz

---
+++ test.js
@@ -0,0 +1,9 @@
+const weather = require('./index.js');
+weather('odessa, ua');
+
+// ======================================================================================================
+// const move = async () => {
+//     const data = await weather('odessa, ua');
+//     console.log(data);
+// }
+// move();
